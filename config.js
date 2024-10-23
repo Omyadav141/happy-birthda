278e390a63e7ab341c09b2b09ed22787025265f5
Om G. Yadav <omyadav9860@gmail.com>
2024-10-24 00:14:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "i know you will spend this year amazing , Haapy birthday sachiiiii ",
+    ],
+    
+   
+    desc: {
+        turn_on: "turn_on",
+        play: "play",
+        bannar_coming: "Let's decorate",
+        balloons_flying: "somthing missing",
+        cake_fadein: "Something i cook for you ",
+        light_candle: "blow the candle",
+        wish_message: "Happy Birthday Sachii 💗✨",
+        story: "A MESSAGE FOR YOU",
+    }
+};
